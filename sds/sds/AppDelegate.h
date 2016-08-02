6fd758632c91571c7730dcce7cfc86dc60079a78
Venkat <venkat.ramana@ymedialabs.com>
2016-08-02 16:02:06 +0530

    push changes

---
+++ sds/sds/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sds
+//
+//  Created by Venkat on 8/2/16.
+//  Copyright © 2016 ymedialabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sds/sds/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sds
+//
+//  Created by Venkat on 8/2/16.
+//  Copyright © 2016 ymedialabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
